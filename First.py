dbe2028a24d9c0d5fb164fa0decadf066713e8cb
ButterChick <dhruvphooli@gmail.com>
2023-03-08 23:52:48 +0530

    FInally Done with the first exercise

--- First.py
+++ First.py
@@ -15,23 +15,36 @@ d = {'k1':[1,2,3,{'tricky':['oh','man','inception',{'target':[1,2,3,'hello']}]}]
 print(d['k1'][3]['tricky'][3]['target'][3])
 
 mail ="user@domain.com"
-def getdomain(x):
-    flag = 0
-    for i in x:
-        if i == chr(64):
-            flag = 1
-        if flag == 1:
-            print(i)
+def getdomain(mail):
+    print(mail.split('@')[-1])
 
 getdomain(mail)
 
 def findDog(x):
-    s = x.split()
-    for i in s:
-        if s in ["dog","Dog","DOG"]:
-            print("True")
-            break
-        else:
-            print("Flase")
+    return 'dog' in x.lower().split()
 
-findDog('dog')
+print(findDog('dog'))
+
+count = 0
+
+for i in 'This dog runs faster than the other dog dude!'.split():
+    if i == 'dog':
+        count += 1
+
+print(count)
+
+seq = ['soup','dog','salad','cat','great']
+print(list(filter(lambda word: word[0]=='s',seq)))
+
+def caughtspeeding(speed, is_birthday):
+    if is_birthday == True:
+        speed -= 5
+    if speed <= 60:
+        print("No ticket")
+    elif speed > 61 and speed <= 80:
+        print("Small Ticket")
+    else:
+        print("Big ticket")
+
+caughtspeeding(81,True)
+caughtspeeding(81,False)
